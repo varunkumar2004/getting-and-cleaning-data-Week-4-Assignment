1313e6d52ee7a0fd026b51fd629371393ed92e7f
varun <varunkumar2004.vk@gmail.com>
2024-11-10 12:01:10 +0530

    changes to run_alalysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,6 @@ dataPath <- "UCI HAR Dataset"
 trainingSubjects <- read.table(file.path(dataPath, "train", "subject_train.txt"))
 trainingValues <- read.table(file.path(dataPath, "train", "X_train.txt"))
 trainingActivity <- read.table(file.path(dataPath, "train", "y_train.txt"))
-
 # read test data
 testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
 testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
@@ -14,40 +13,27 @@ testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
 
 # read features
 features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
-
 # read activity labels
 activities <- read.table(file.path(dataPath, "activity_labels.txt"))
 colnames(activities) <- c("activityId", "activityLabel")
 
-##############################################################################
-# Step 1 - Merge the training and the test sets to create one data set
-##############################################################################
+## Merge the training and the test sets to create one data set
 humanActivity <- rbind(
   cbind(trainingSubjects, trainingValues, trainingActivity),
   cbind(testSubjects, testValues, testActivity)
 )
-
 # assign column names
 colnames(humanActivity) <- c("subject", features[, 2], "activity")
 
-################################################################################################
-# Step 2 - Extract only the measurements on the mean and standard deviation for each measurement
-################################################################################################
-
+## Extract only the measurements on the mean and standard deviation for each measurement
 columnsToKeep <- grepl("subject|activity|mean|std", colnames(humanActivity))
 humanActivity <- humanActivity[, columnsToKeep]
 
-#################################################################################
-# Step 3 - Use descriptive activity names to name the activities in the data set
-#################################################################################
-
+## Use descriptive activity names to name the activities in the data set
 humanActivity$activity <- factor(humanActivity$activity,levels = activities[, 1], labels = activities[, 2])
 humanActivity$activity
 
-##############################################################################
-# Step 4 - Appropriately label the data set with descriptive variable names
-##############################################################################
-
+## Appropriately label the data set with descriptive variable names
 humanActivityCols <- colnames(humanActivity)
 humanActivityCols <- gsub("[\\(\\)-]", "", humanActivityCols)
 humanActivityCols <- gsub("^f", "frequencyDomain", humanActivityCols)
@@ -58,20 +44,13 @@ humanActivityCols <- gsub("Mag", "Magnitude", humanActivityCols)
 humanActivityCols <- gsub("Freq", "Frequency", humanActivityCols)
 humanActivityCols <- gsub("mean", "Mean", humanActivityCols)
 humanActivityCols <- gsub("std", "StandardDeviation", humanActivityCols)
-
 humanActivityCols <- gsub("BodyBody", "Body", humanActivityCols)
 colnames(humanActivity) <- humanActivityCols
-
 colnames(humanActivity)
 
-######################################################################################################################
-# Step 5 - Create a second, independent tidy set with the average of each variable for each activity and each subject
-######################################################################################################################
-
-humanActivityMeans <- humanActivity %>% 
+## Create a second, independent tidy set with the average of each variable for each activity and each subjecthumanActivityMeans <- humanActivity %>% 
   group_by(subject, activity) %>%
   summarise_each(funs(mean))
-
 # output to file "tidy_data.txt"
 write.table(humanActivityMeans, "tidy_data.txt", row.names = FALSE, 
             quote = FALSE)
